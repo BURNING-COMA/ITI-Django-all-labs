89474a99497b0f8eef7fbb133021ef85410a5f73
BURING-COMA <xmanhycal@gmail.com>
2022-02-06 12:12:23 +0200

    register intake and track models in admin panel

--- lab4/amazon/admin.py
+++ lab4/amazon/admin.py
@@ -5,6 +5,7 @@ from . import models
 
 admin.site.register(models.MyUser)
 admin.site.register(models.Student)
+# register track and intake models 
 admin.site.register(models.Track)
 admin.site.register(models.Intake)
 
